a0c10c6710cbfd357e5afe559c0f79a46191c212
liukai <liukai@pjlab.org.cn>
2022-11-14 10:16:12 +0800

    merge exp-pruning

--- mmrazor/models/task_modules/tracer/prune_tracer.py
+++ mmrazor/models/task_modules/tracer/prune_tracer.py
@@ -1,35 +1,32 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
+import copy
+from typing import Dict, List
+
+import torch
 import torch.nn as nn
-from mmengine.model import BaseModel, revert_sync_batchnorm
+from mmengine.utils.dl_utils import mmcv_full_available
 
 from mmrazor.models.architectures.dynamic_ops import DynamicChannelMixin
+from mmrazor.models.mutables.mutable_channel import (
+    MutableChannelUnit, SequentialMutableChannelUnit)
+from mmrazor.models.mutables.mutable_channel.units.utils import find_mutable
 from mmrazor.registry import TASK_UTILS
 from mmrazor.structures.graph import BaseGraph, ModuleGraph
 from mmrazor.structures.graph.channel_graph import (
     ChannelGraph, default_channel_node_converter)
 from mmrazor.structures.graph.module_graph import (FxTracerToGraphConverter,
                                                    PathToGraphConverter)
-from mmrazor.utils import get_placeholder
+from mmrazor.utils import demo_inputs
 from .backward_tracer import BackwardTracer
 from .fx_tracer import CustomFxTracer
 from .loss_calculator.sum_loss_calculator import SumPseudoLoss
 from .razor_tracer import FxBaseNode, RazorFxTracer
-
-try:
-    from mmdet.models import BaseDetector
-except Exception:
-    BaseDetector = get_placeholder('mmdet')
-
-try:
-    from mmcls.models import ImageClassifier
-except Exception:
-    ImageClassifier = get_placeholder('mmcls')
 """
 - How to config PruneTracer using hard code
   - fxtracer
     - concrete args
-      - PruneTracer.default_concrete_args_fun
+      - demo_inputs
     - leaf module
       - PruneTracer.default_leaf_modules
     - method
@@ -43,21 +40,48 @@ except Exception:
 # concrete args
 
 
-def default_mm_concrete_args(model, input_shape):
-    return {'mode': 'tensor'}
+def revert_sync_batchnorm(module: nn.Module) -> nn.Module:
+    """Helper function to convert all `SyncBatchNorm` (SyncBN) and
+    `mmcv.ops.sync_bn.SyncBatchNorm`(MMSyncBN) layers in the model to
+    `BatchNormXd` layers.
 
+    Adapted from @kapily's work:
+    (https://github.com/pytorch/pytorch/issues/41081#issuecomment-783961547)
 
-def default_concrete_args(model, input_shape):
-    return {}
+    Args:
+        module (nn.Module): The module containing `SyncBatchNorm` layers.
 
+    Returns:
+        module_output: The converted module with `BatchNormXd` layers.
+    """
+    module_output = module
+    module_checklist = [torch.nn.modules.batchnorm.SyncBatchNorm]
 
-def det_concrete_args(model, input_shape):
-    assert isinstance(model, BaseDetector)
-    from mmdet.testing._utils import demo_mm_inputs
-    data = demo_mm_inputs(1, [input_shape[1:]])
-    data = model.data_preprocessor(data, False)
-    data.pop('inputs')
-    return data
+    if mmcv_full_available():
+        from mmcv.ops import SyncBatchNorm
+        module_checklist.append(SyncBatchNorm)
+
+    if isinstance(module, tuple(module_checklist)):
+        module_output = nn.BatchNorm2d(module.num_features, module.eps,
+                                       module.momentum, module.affine,
+                                       module.track_running_stats)
+        if module.affine:
+            # no_grad() may not be needed here but
+            # just to be consistent with `convert_sync_batchnorm()`
+            with torch.no_grad():
+                module_output.weight = module.weight
+                module_output.bias = module.bias
+        module_output.running_mean = module.running_mean
+        module_output.running_var = module.running_var
+        module_output.num_batches_tracked = module.num_batches_tracked
+        module_output.training = module.training
+        # qconfig exists in quantized models
+        if hasattr(module, 'qconfig'):
+            module_output.qconfig = module.qconfig
+    for name, child in module.named_children():
+        module_output.add_module(name, revert_sync_batchnorm(child))
+    del module
+    return module_output
 
 
 @TASK_UTILS.register_module()
@@ -74,12 +98,6 @@ class PruneTracer:
         # mmcv
         Scale,
     )
-    default_concrete_args_fun = {
-        BaseDetector: det_concrete_args,
-        ImageClassifier: default_mm_concrete_args,
-        BaseModel: default_mm_concrete_args,
-        nn.Module: default_concrete_args
-    }
 
     def __init__(self,
                  input_shape=(1, 3, 224, 224),
@@ -128,24 +146,39 @@ class PruneTracer:
         channel_graph.forward(self.input_shape[1])
         unit_configs = channel_graph.generate_units_config()
 
-        return unit_configs
+        return self._find_mutable_units(model, unit_configs)
 
     def _fx_trace(self, model):
-        args = self.get_concrete_args(model)
-        return self.tracer.trace(model, concrete_args=args)
+        args = demo_inputs(model, self.input_shape)
+        if isinstance(args, dict):
+            args.pop('inputs')
+            return self.tracer.trace(model, concrete_args=args)
+        else:
+            return self.tracer.trace(model)
 
-    def get_concrete_args(self, model):
-        for module_type, concrete_args_fun in self.default_concrete_args_fun.items(  # noqa
-        ):  # noqa
-            if isinstance(model, module_type):
-                return concrete_args_fun(model, self.input_shape)
-        return {}
+    def _find_mutable_units(self, model, units_config: Dict):
+        model = copy.deepcopy(model)
+        units: List[SequentialMutableChannelUnit] = [
+            SequentialMutableChannelUnit.init_from_cfg(model, cfg)
+            for cfg in units_config.values()
+        ]
+        for unit in units:
+            unit.prepare_for_pruning(model)
+        mutable_units = [unit for unit in units if unit.is_mutable]
+        inputs = demo_inputs(model, [1, 3, 224, 224])
+        model.eval()
 
-    def _det_input(self, model):
-        assert isinstance(model, BaseDetector)
-        from mmdet.testing._utils import demo_mm_inputs
-        data = demo_mm_inputs(1, [self.input_shape[1:]])
-        data = model.data_preprocessor(data, False)
-        data['mode'] = 'tensor'
-        data.pop('inputs')
-        return data
+        if isinstance(inputs, dict):
+            inputs['mode'] = 'loss'
+            template_output = model(**inputs)
+        else:
+            template_output = model(inputs)
+
+        mutable_units = find_mutable(model, mutable_units, units,
+                                     template_output)
+        mutable_unit_config = {}
+        for unit in mutable_units:
+            mutable_unit_config[
+                unit.name] = MutableChannelUnit.config_template(
+                    unit, with_channels=True, with_init_args=True)
+        return mutable_unit_config
