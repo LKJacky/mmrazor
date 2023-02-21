d7e1e1c107258e6c21e75776721dab2e06babd9f
jacky <jacky@xx.com>
2023-02-22 00:35:19 +0800

    update scripts

--- projects/algorithms/dtp/configs/mmcls/vgg/imp/script.sh
+++ projects/algorithms/dtp/configs/mmcls/vgg/imp/script.sh
@@ -1,3 +1,3 @@
 export JOB_NAME=dtp_prune_vgg_revert
-python ./tools/train.py projects/algorithms/dtp/configs/mmcls/vgg/prune_iter_ratio/${JOB_NAME}.py
-python ./tools/train.py projects/algorithms/dtp/configs/mmcls/vgg/prune_iter_ratio/dtp_finetune_vgg.py --work-dir ./work_dirs/$JOB_NAME
+python ./tools/train.py projects/algorithms/dtp/configs/mmcls/vgg/imp/${JOB_NAME}.py
+python ./tools/train.py projects/algorithms/dtp/configs/mmcls/vgg/dtp_finetune_vgg.py --work-dir ./work_dirs/$JOB_NAME
--- projects/algorithms/dtp/configs/mmcls/vgg/prune_schedule/script.sh
+++ projects/algorithms/dtp/configs/mmcls/vgg/prune_schedule/script.sh
@@ -1,3 +1,3 @@
 export JOB_NAME=dtp_prune_vgg_r9_fix
-python ./tools/train.py projects/algorithms/dtp/configs/mmcls/vgg/prune_iter_ratio/${JOB_NAME}.py
-python ./tools/train.py projects/algorithms/dtp/configs/mmcls/vgg/prune_iter_ratio/dtp_finetune_vgg.py --work-dir ./work_dirs/$JOB_NAME
+python ./tools/train.py projects/algorithms/dtp/configs/mmcls/vgg/prune_schedule//${JOB_NAME}.py
+python ./tools/train.py projects/algorithms/dtp/configs/mmcls/vgg/dtp_finetune_vgg.py --work-dir ./work_dirs/$JOB_NAME
