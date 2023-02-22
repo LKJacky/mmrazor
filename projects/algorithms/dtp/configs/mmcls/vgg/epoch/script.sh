37222b1ace3e6f2956ab49ea5443f74b27ac3d37
jacky <jacky@xx.com>
2023-02-22 22:27:26 +0800

    update

---
+++ projects/algorithms/dtp/configs/mmcls/vgg/epoch/script.sh
@@ -0,0 +1,3 @@
+export JOB_NAME=dtp_prune_vgg_revert
+python ./tools/train.py projects/algorithms/dtp/configs/mmcls/vgg/imp/${JOB_NAME}.py
+python ./tools/train.py projects/algorithms/dtp/configs/mmcls/vgg/dtp_finetune_vgg.py --work-dir ./work_dirs/${JOB_NAME}_finetune
