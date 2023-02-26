62d360ee518d1930be5958a59db811f189a39d20
jacky <jacky@xx.com>
2023-02-26 16:12:53 +0800

    add config

--- projects/models/resnet50/resnet50_nvidia.py
+++ projects/models/resnet50/resnet50_nvidia.py
@@ -1,125 +1,57 @@
-# type: ignore
-model = dict(
-    type='ImageClassifier',
-    backbone=dict(
-        type='ResNet',
-        depth=50,
-        num_stages=4,
-        out_indices=(3, ),
-        style='pytorch'),
-    neck=dict(type='GlobalAveragePooling'),
-    head=dict(
-        type='LinearClsHead',
-        num_classes=1000,
-        in_channels=2048,
-        loss=dict(
-            type='LabelSmoothLoss', loss_weight=1.0, label_smooth_val=0.1),
-        topk=(1, 5)),
-    _scope_='mmcls')
-dataset_type = 'ImageNet'
-data_preprocessor = dict(
-    num_classes=1000,
-    mean=[123.675, 116.28, 103.53],
-    std=[58.395, 57.12, 57.375],
-    to_rgb=True)
-train_pipeline = [
-    dict(type='LoadImageFromFile', _scope_='mmcls'),
-    dict(type='RandomResizedCrop', scale=224, _scope_='mmcls'),
-    dict(type='RandomFlip', prob=0.5, direction='horizontal', _scope_='mmcls'),
-    dict(type='PackClsInputs', _scope_='mmcls')
+_base_ = [
+    'mmcls::_base_/schedules/imagenet_bs2048_coslr.py',
+    'mmcls::_base_/models/resnet50.py',
+    'mmcls::_base_/datasets/imagenet_bs32.py',
+    'mmcls::_base_/default_runtime.py',
 ]
-test_pipeline = [
-    dict(type='LoadImageFromFile', _scope_='mmcls'),
-    dict(type='ResizeEdge', scale=256, edge='short', _scope_='mmcls'),
-    dict(type='CenterCrop', crop_size=224, _scope_='mmcls'),
-    dict(type='PackClsInputs', _scope_='mmcls')
-]
-train_dataloader = dict(
-    batch_size=32,
-    num_workers=5,
-    dataset=dict(
-        type='ImageNet',
-        data_root='data/imagenet',
-        ann_file='meta/train.txt',
-        data_prefix='train',
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(type='RandomResizedCrop', scale=224),
-            dict(type='RandomFlip', prob=0.5, direction='horizontal'),
-            dict(type='PackClsInputs')
-        ],
-        _scope_='mmcls'),
-    sampler=dict(type='DefaultSampler', shuffle=True, _scope_='mmcls'))
-val_dataloader = dict(
-    batch_size=32,
-    num_workers=5,
-    dataset=dict(
-        type='ImageNet',
-        data_root='data/imagenet',
-        ann_file='meta/val.txt',
-        data_prefix='val',
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(type='ResizeEdge', scale=256, edge='short'),
-            dict(type='CenterCrop', crop_size=224),
-            dict(type='PackClsInputs')
-        ],
-        _scope_='mmcls'),
-    sampler=dict(type='DefaultSampler', shuffle=False, _scope_='mmcls'))
-val_evaluator = dict(type='Accuracy', topk=(1, 5), _scope_='mmcls')
-test_dataloader = dict(
-    batch_size=32,
-    num_workers=5,
-    dataset=dict(
-        type='ImageNet',
-        data_root='data/imagenet',
-        ann_file='meta/val.txt',
-        data_prefix='val',
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(type='ResizeEdge', scale=256, edge='short'),
-            dict(type='CenterCrop', crop_size=224),
-            dict(type='PackClsInputs')
-        ],
-        _scope_='mmcls'),
-    sampler=dict(type='DefaultSampler', shuffle=False, _scope_='mmcls'))
-test_evaluator = dict(type='Accuracy', topk=(1, 5), _scope_='mmcls')
+
+# batch size
+batch_size = 256
+train_dataloader = dict(batch_size=batch_size, )
+val_dataloader = dict(batch_size=batch_size, )
+
+# disable bn weight decay
+paramwise_cfg = dict(custom_keys={
+    'mutable_channel': dict(decay_mult=0.0),
+    'bn': dict(decay_mult=0.0)
+})
+# turn lr
 optim_wrapper = dict(
     optimizer=dict(
-        type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001,
-        _scope_='mmcls'))
-param_scheduler = dict(
-    type='CosineAnnealingLR',
-    T_max=140,
-    by_epoch=True,
-    begin=0,
-    end=140,
-    _scope_='mmcls')
-train_cfg = dict(by_epoch=True, max_epochs=140, val_interval=1)
-val_cfg = dict()
-test_cfg = dict()
-auto_scale_lr = dict(base_batch_size=256)
-default_scope = 'mmcls'
-default_hooks = dict(
-    timer=dict(type='IterTimerHook', _scope_='mmcls'),
-    logger=dict(type='LoggerHook', interval=100, _scope_='mmcls'),
-    param_scheduler=dict(type='ParamSchedulerHook', _scope_='mmcls'),
-    checkpoint=dict(type='CheckpointHook', interval=1, _scope_='mmcls'),
-    sampler_seed=dict(type='DistSamplerSeedHook', _scope_='mmcls'),
-    visualization=dict(
-        type='VisualizationHook', enable=False, _scope_='mmcls'))
-env_cfg = dict(
-    cudnn_benchmark=False,
-    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
-    dist_cfg=dict(backend='nccl'))
-vis_backends = [dict(type='LocalVisBackend', _scope_='mmcls')]
-visualizer = dict(
-    type='ClsVisualizer',
-    vis_backends=[dict(type='LocalVisBackend')],
-    _scope_='mmcls')
-log_level = 'INFO'
-load_from = None
-resume = False
-randomness = dict(seed=None, deterministic=False)
-launcher = 'none'
-work_dir = './work_dirs/resnet50_pretrain_cos_smooth'
+        type='SGD',
+        lr=0.256 * 8,
+        momentum=0.875,
+        weight_decay=3.0517578125e-05,
+        nesterov=False),
+    paramwise_cfg=paramwise_cfg)
+
+# change epoch
+epoch = 256
+param_scheduler = [
+    # warm up learning rate scheduler
+    dict(
+        type='LinearLR',
+        start_factor=1 / 8,
+        by_epoch=True,
+        begin=0,
+        # about 2500 iterations for ImageNet-1k
+        end=8,
+        # update by iter
+        convert_to_iter_based=False),
+    # main learning rate scheduler
+    dict(
+        type='CosineAnnealingLR',
+        T_max=epoch - 8,
+        by_epoch=True,
+        begin=8,
+        end=epoch,
+    )
+]
+
+train_cfg = dict(max_epochs=epoch)
+
+# add label smooth
+model = dict(
+    head=dict(
+        loss=dict(
+            type='LabelSmoothLoss', label_smooth_val=0.1, loss_weight=1.0), ))
