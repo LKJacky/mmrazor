28fe23d0e6072b4daacd1c31c126e733b8d439f6
Your Name <you@example.com>
2023-02-27 13:39:38 +0800

    add finetune lr

--- projects/algorithms/dtp/configs/mmcls/resnet50/dtp_finetune_r50.py
+++ projects/algorithms/dtp/configs/mmcls/resnet50/dtp_finetune_r50.py
@@ -13,7 +13,13 @@ param_scheduler = dict(
     end=epoch,
     _scope_='mmcls')
 train_cfg = dict(by_epoch=True, max_epochs=epoch, val_interval=1)
+finetune_lr = 0.1
 ##############################################################################
+
+optim_wrapper = _base_.optim_wrapper
+optim_wrapper.update({
+    'optimizer': dict(lr=finetune_lr),
+})
 
 algorithm = _base_.model
 algorithm.init_cfg = dict(type='Pretrained', checkpoint=pruned_path)
