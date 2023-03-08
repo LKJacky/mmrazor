690f505d582f0f1f6f46850c1f7fdfc2aca93fc0
jacky <jacky@xx.com>
2023-03-08 20:06:25 +0800

    add config

--- projects/configs/dtp/dtp_vgg/task.sh
+++ projects/configs/dtp/dtp_vgg/task.sh
@@ -7,3 +7,8 @@ export JOB_NAME=dtp_vgg_prune_30
 export PTH_NAME=epoch_15
 python ./tools/train.py projects/configs/dtp/dtp_vgg/dtp_vgg_prune_30.py
 python ./tools/train.py projects/configs/dtp/dtp_vgg/dtp_vgg_finetune.py
+
+export JOB_NAME=dtp_vgg_prune_20
+export PTH_NAME=epoch_15
+python ./tools/train.py projects/configs/dtp/dtp_vgg/dtp_vgg_prune_20.py
+python ./tools/train.py projects/configs/dtp/dtp_vgg/dtp_vgg_finetune.py
