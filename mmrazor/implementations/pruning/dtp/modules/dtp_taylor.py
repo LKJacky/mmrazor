710122d1676a023fab6685e1897576e0624461a9
jacky <jacky@xx.com>
2023-03-10 00:51:19 +0800

    fix bug

--- mmrazor/implementations/pruning/dtp/modules/dtp_taylor.py
+++ mmrazor/implementations/pruning/dtp/modules/dtp_taylor.py
@@ -11,10 +11,10 @@ from .unit import BaseDTPUnit
 # dtp with taylor importance base dtp with adaptive importance
 
 
-def taylor_backward_hook_wrapper(module: 'DTPTMutableChannelImp'):
+def taylor_backward_hook_wrapper(module: 'DTPTMutableChannelImp', input):
 
     def taylor_backward_hook(grad):
-        module.update_taylor(grad)
+        module.update_taylor(input, grad)
 
     return taylor_backward_hook
 
@@ -27,32 +27,35 @@ class DTPTMutableChannelImp(BaseDTPMutableChannel):
         self.e = nn.parameter.Parameter(
             torch.tensor([1.0]), requires_grad=False)
 
-        self.imp = nn.parameter.Parameter(torch.ones([num_channels]))
-
         taylor = torch.zeros([num_channels])
         self.register_buffer('taylor', taylor)
         self.taylor: torch.Tensor
 
-        self.imp.register_hook(taylor_backward_hook_wrapper(self))
         self.decay = 0.99
         self.requires_grad_(False)
 
     @property
     def current_imp(self):
         e_imp = dtp_get_importance(self.taylor, self.e)
-        w = e_imp * self.imp
+        if self.training and e_imp.requires_grad:
+            e_imp.register_hook(taylor_backward_hook_wrapper(self, e_imp))
         if self.training:
             with torch.no_grad():
                 self.mask.data = (e_imp >= 0.5).float()
-        return w
+        return e_imp
+
+    @property
+    def current_imp_flop(self):
+        e_imp = dtp_get_importance(self.taylor, self.e)
+        return e_imp
 
     @torch.no_grad()
     def limit_value(self):
         self.e.data = torch.clamp(self.e, 1 / self.num_channels, 1.0)
 
     @torch.no_grad()
-    def update_taylor(self, grad):
-        new_taylor = (self.imp * grad)**2
+    def update_taylor(self, input, grad):
+        new_taylor = (input * grad)**2
         all_reduce(new_taylor)
         self.taylor = self.taylor * self.decay + (1 - self.decay) * new_taylor
 
@@ -71,8 +74,6 @@ class DTPTUnit(BaseDTPUnit):
 
     @torch.no_grad()
     def info(self) -> str:
-        return (f'imp gate: {self.mutable_channel.imp.min().item():.3f}\t'
-                f'{self.mutable_channel.imp.max().item():.3f}\t'
-                f'taylor: {self.mutable_channel.taylor.min().item():.3f}\t'
+        return (f'taylor: {self.mutable_channel.taylor.min().item():.3f}\t'
                 f'{self.mutable_channel.taylor.max().item():.3f}\t'
                 f'e: {self.mutable_channel.e.item():.3f}')  # noqa
