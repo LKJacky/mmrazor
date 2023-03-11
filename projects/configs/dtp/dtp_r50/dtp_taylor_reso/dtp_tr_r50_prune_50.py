a6f9b51f0172c8dd731aac70ec602edc1678d5da
jacky <jacky@xx.com>
2023-03-12 01:23:39 +0800

    add configs

---
+++ projects/configs/dtp/dtp_r50/dtp_taylor_reso/dtp_tr_r50_prune_50.py
@@ -0,0 +1,80 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+#############################################################################
+_base_ = '../../../resnet50/resnet50_pretrain_cos_smooth.py'
+pretrained_path = 'work_dirs/pretrained/resnet50_cos_smooth_140.pth'
+
+decay_ratio = 0.6
+refine_ratio = 0.4
+target_flop_ratio = 0.5
+flop_loss_weight = 100
+
+log_interval = 1000
+
+input_shape = (1, 3, 224, 224)
+
+epoch = 140
+train_cfg = dict(by_epoch=True, max_epochs=epoch)
+
+mutator_lr = 0.01
+original_lr = _base_.optim_wrapper.optimizer.lr
+##############################################################################
+
+custom_imports = dict(imports=['projects'])
+
+architecture = _base_.model
+
+if hasattr(_base_, 'data_preprocessor'):
+    architecture.update({'data_preprocessor': _base_.data_preprocessor})
+    data_preprocessor = None
+
+architecture.init_cfg = dict(type='Pretrained', checkpoint=pretrained_path)
+architecture['_scope_'] = _base_.default_scope
+
+model = dict(
+    _delete_=True,
+    _scope_='mmrazor',
+    type='BaseDTPAlgorithm',
+    architecture=architecture,
+    mutator_cfg=dict(
+        type='DTPTRMutator',
+        channel_unit_cfg=dict(type='DTPTRUnit', default_args=dict()),
+        parse_cfg=dict(
+            _scope_='mmrazor',
+            type='ChannelAnalyzer',
+            demo_input=dict(
+                type='DefaultDemoInput',
+                input_shape=input_shape,
+            ),
+            tracer_type='FxTracer'),
+    ),
+    scheduler=dict(
+        type='DTPTRScheduler',
+        flops_target=target_flop_ratio,
+        decay_ratio=decay_ratio,
+        refine_ratio=refine_ratio,
+        flop_loss_weight=flop_loss_weight,
+        structure_log_interval=log_interval),
+)
+custom_hooks = getattr(_base_, 'custom_hooks', []) + [
+    dict(
+        type='mmrazor.PruningStructureHook',
+        by_epoch=False,
+        interval=log_interval),
+    dict(
+        type='mmrazor.ResourceInfoHook',
+        interval=1,
+        demo_input=dict(
+            type='mmrazor.DefaultDemoInput',
+            input_shape=input_shape,
+        ),
+        early_stop=False,
+        save_ckpt_thr=[],
+    ),
+]
+paramwise_cfg = dict(custom_keys={
+    'mutable_channel': dict(decay_mult=0.0, lr=mutator_lr),
+})
+optim_wrapper = _base_.optim_wrapper
+optim_wrapper.update({
+    'paramwise_cfg': paramwise_cfg,
+})
