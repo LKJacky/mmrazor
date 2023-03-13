fd75dfb20fccef7258db9a4a0d3275c9a0660dac
Your Name <you@example.com>
2023-03-13 19:00:17 +0800

    add cofnig

--- projects/configs/dtp/dtp_r50/dtp_taylor/task.sh
+++ projects/configs/dtp/dtp_r50/dtp_taylor/task.sh
@@ -13,3 +13,8 @@ export JOB_NAME=dtp_t_r50_prune_50_cls
 export PTH_NAME=epoch_9
 bash ./tools/dist_train.sh projects/configs/dtp/dtp_r50/dtp_taylor/dtp_t_r50_prune_50_cls.py 8
 bash ./tools/dist_train.sh projects/configs/dtp/dtp_r50/dtp_r50_finetune_cls.py 8 --work-dir work_dirs/${JOB_NAME}_finetune
+
+export JOB_NAME=dtp_t_r50_prune_50_cls_90
+export PTH_NAME=epoch_90
+bash ./tools/dist_train.sh projects/configs/dtp/dtp_r50/dtp_taylor/dtp_t_r50_prune_50_cls_90.py 8
+bash ./tools/dist_train.sh projects/configs/dtp/dtp_r50/dtp_r50_finetune_cls.py 8 --work-dir work_dirs/${JOB_NAME}_finetune
