f38489ffb0511648ac79cc093de007ab31dcde68
jacky <jacky@xx.com>
2023-03-19 17:33:13 +0800

    add config

---
+++ projects/configs/dms/dms_r50/dms_r101/task.sh
@@ -0,0 +1,4 @@
+export JOB_NAME=dms_t_r101_prune_25_cls
+export PTH_NAME=epoch_10
+bash ./tools/dist_train.sh projects/configs/dms/dms_r50/dms_r101/dms_t_r101_prune_25_cls.py 8
+bash ./tools/dist_train.sh projects/configs/dms/dms_r50/dms_r101/dms_t_r101_finetune_cls.py 8 --work-dir work_dirs/${JOB_NAME}_finetune
