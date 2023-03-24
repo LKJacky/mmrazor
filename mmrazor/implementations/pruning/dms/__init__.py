220f330b57f7526bdd61751835e3b43310af19a4
jacky <jacky@xx.com>
2023-03-24 15:18:27 +0800

    add dms sq

--- mmrazor/implementations/pruning/dms/__init__.py
+++ mmrazor/implementations/pruning/dms/__init__.py
@@ -1,3 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .core import *  # noqa
 from .dms_c import *  # noqa
+from .dms_sq import *  # noqa
---
+++ mmrazor/implementations/pruning/dms/dms_sq.py
@@ -0,0 +1,49 @@
+# dms by channel
+
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+
+from mmrazor.registry import MODELS
+from ..dtp.modules.dtp_taylor import dtopk
+from .core.mutable import MutableBlocks
+from .core.mutator import BlockInitialer, DMSMutator
+from .core.op import DynamicStage
+
+# dms sequentially.
+
+
+class SqMutableBlocks(MutableBlocks):
+
+    def __init__(self, num_blocks) -> None:
+        super().__init__(num_blocks)
+        self.taylor = torch.linspace(0, 1, self.num_blocks)
+
+    @property
+    def current_imp(self):
+        imp = dtopk(self.taylor, self.e, lamda=4.0)
+
+        if self.training and imp.requires_grad:
+            with torch.no_grad():
+                self.mask.data = (imp >= 0.5).float()
+        return imp
+
+
+class SqDynamicStaget(DynamicStage):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        self.register_mutable_attr(
+            'mutable_blocks', SqMutableBlocks(len(list(self.removable_block))))
+        self.mutable_blocks: SqMutableBlocks
+
+        self.prepare_blocks()
+
+
+@MODELS.register_module()
+class DMS_Sq_CMutator(DMSMutator):
+
+    def __init__(self, *args, **kwargs):
+        DMSMutator.__init__(self, *args, **kwargs)
+        self.block_initializer = BlockInitialer(
+            dynamic_statge_module=SqDynamicStaget)
