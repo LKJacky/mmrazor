172281037ccea81d0e6c5feee17090f27b01e870
nn <nn>
2023-03-25 08:15:12 +0800

    add config

--- projects/configs/dms/dms_r56/dms_lr/task.sh
+++ projects/configs/dms/dms_r56/dms_lr/task.sh
@@ -24,3 +24,8 @@ export JOB_NAME=dms_r56super_prune_e_lrc_tscos
 export PTH_NAME=epoch_100
 python ./tools/train.py projects/configs/dms/dms_r56/dms_lr/dms_r56super_prune_e_lrc_tscos.py
 python ./tools/train.py projects/configs/dms/dms_r56/dms_r56super_finetune.py --work-dir work_dirs/${JOB_NAME}_finetune
+
+export JOB_NAME=dms_r56super_prune_e_lrc_tscos_ep300
+export PTH_NAME=epoch_300
+python ./tools/train.py projects/configs/dms/dms_r56/dms_lr/dms_r56super_prune_e_lrc_tscos_ep300.py
+python ./tools/train.py projects/configs/dms/dms_r56/dms_r56super_finetune.py --work-dir work_dirs/${JOB_NAME}_finetune
