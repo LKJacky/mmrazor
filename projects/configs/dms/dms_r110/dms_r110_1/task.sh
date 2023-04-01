aa1ab0bb3c72bd936979023dfb759f3a336e7ac2
jacky <jacky@xx.com>
2023-04-02 00:59:26 +0800

    add config

--- projects/configs/dms/dms_r110/dms_r110_1/task.sh
+++ projects/configs/dms/dms_r110/dms_r110_1/task.sh
@@ -25,3 +25,15 @@ export JOB_NAME=${EXP_NAME}/dms_r110_fix_l2_loop10
 export PTH_NAME=epoch_100
 python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_1/dms_r110_fix_l2_loop10.py --work-dir work_dirs/${JOB_NAME}
 python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_1/dms_r110_finetune.py --work-dir work_dirs/${JOB_NAME}_finetune
+
+
+export JOB_NAME=${EXP_NAME}/dms_r110_fix_l2_loop30
+export PTH_NAME=epoch_100
+python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_1/dms_r110_fix_l2_loop30.py --work-dir work_dirs/${JOB_NAME}
+python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_1/dms_r110_finetune.py --work-dir work_dirs/${JOB_NAME}_finetune
+
+
+export JOB_NAME=${EXP_NAME}/dms_r110_fix_l2_loop60
+export PTH_NAME=epoch_100
+python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_1/dms_r110_fix_l2_loop60.py --work-dir work_dirs/${JOB_NAME}
+python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_1/dms_r110_finetune.py --work-dir work_dirs/${JOB_NAME}_finetune
