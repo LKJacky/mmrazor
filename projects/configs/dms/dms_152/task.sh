1faa565847ce8bea209cd4404db2dd5a14265106
Your Name <you@example.com>
2023-04-03 12:50:07 +0800

    update config

--- projects/configs/dms/dms_152/task.sh
+++ projects/configs/dms/dms_152/task.sh
@@ -6,4 +6,4 @@ bash ./tools/dist_train.sh projects/configs/dms/dms_152/dms_t_r152_finetune_cls.
 export JOB_NAME=dtp_t_r152_prune_17_cls
 export PTH_NAME=epoch_10
 bash ./tools/dist_train.sh projects/configs/dms/dms_152/dtp_t_r152_prune_17_cls.py 8
-bash ./tools/dist_train.sh projects/configs/dms/dms_152/dms_t_r152_finetune_cls.py 8 --work-dir work_dirs/${JOB_NAME}_finetune
+bash ./tools/dist_train.sh projects/configs/dms/dms_152/dtp_t_r152_finetune_17_cls.py 8 --work-dir work_dirs/${JOB_NAME}_finetune
