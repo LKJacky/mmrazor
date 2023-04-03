7aa084359169e72f5f0c8e10cb4151b7c26a8cd4
jacky <jacky@xx.com>
2023-04-03 17:32:51 +0800

    add config

---
+++ projects/configs/dms/dms_r110/dms_r110_to_56/task.sh
@@ -0,0 +1,7 @@
+
+export EXP_NAME=dms_r110_to_56
+
+export JOB_NAME=${EXP_NAME}/dms_r110_fix_l2_49
+export PTH_NAME=epoch_100
+python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_to_56/dms_r110_fix_l2_49.py --work-dir work_dirs/${JOB_NAME}
+python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_1/dms_r110_finetune.py --work-dir work_dirs/${JOB_NAME}_finetune
