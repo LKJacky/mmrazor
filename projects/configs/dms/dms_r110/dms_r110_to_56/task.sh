c66d266fe650349064359d8f56ee4a45df1381ae
jacky <jacky@xx.com>
2023-04-04 00:56:16 +0800

    update

--- projects/configs/dms/dms_r110/dms_r110_to_56/task.sh
+++ projects/configs/dms/dms_r110/dms_r110_to_56/task.sh
@@ -5,3 +5,8 @@ export JOB_NAME=${EXP_NAME}/dms_r110_fix_l2_49
 export PTH_NAME=epoch_100
 python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_to_56/dms_r110_fix_l2_49.py --work-dir work_dirs/${JOB_NAME}
 python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_1/dms_r110_finetune.py --work-dir work_dirs/${JOB_NAME}_finetune
+
+export JOB_NAME=${EXP_NAME}/dms_r110super_fix_l2_37
+export PTH_NAME=epoch_100
+python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_to_56/dms_r110super_fix_l2_37.py --work-dir work_dirs/${JOB_NAME}
+python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_to_56/dms_r110super_finetune.py --work-dir work_dirs/${JOB_NAME}_finetune
