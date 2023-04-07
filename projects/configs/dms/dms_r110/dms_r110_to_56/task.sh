1b1a4825194e6566b7ff955ae4c38a85ca59e1ce
Your Name <youremail@yourdomain.com>
2023-04-07 09:51:16 +0800

    add configs

--- projects/configs/dms/dms_r110/dms_r110_to_56/task.sh
+++ projects/configs/dms/dms_r110/dms_r110_to_56/task.sh
@@ -10,3 +10,9 @@ export JOB_NAME=${EXP_NAME}/dms_r110super_fix_l2_37
 export PTH_NAME=epoch_100
 python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_to_56/dms_r110super_fix_l2_37.py --work-dir work_dirs/${JOB_NAME}
 python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_to_56/dms_r110super_finetune.py --work-dir work_dirs/${JOB_NAME}_finetune
+
+
+export JOB_NAME=${EXP_NAME}/dms_r110_fix_l2_22
+export PTH_NAME=epoch_100
+python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_to_56/dms_r110_fix_l2_22.py --work-dir work_dirs/${JOB_NAME}
+python ./tools/train.py projects/configs/dms/dms_r110/dms_r110_1/dms_r110_finetune.py --work-dir work_dirs/${JOB_NAME}_finetune
