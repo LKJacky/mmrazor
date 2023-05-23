1e522a8f0e8119459cd05ef96c1bc7887715f22f
Your Name <you@example.com>
2023-05-23 11:56:23 +0800

    add configs

--- projects/configs/dms/ablation/super_size/task.sh
+++ projects/configs/dms/ablation/super_size/task.sh
@@ -2,21 +2,21 @@ export EXP_NAME=dms_ablation_super_size
 # export CPUS_PER_TASK=20
 export SRUN_ARGS="--quotatype=auto"
 
-# export GPUS=2
-# export GPUS_PER_NODE=2
+export GPUS=2
+export GPUS_PER_NODE=2
 
 export JOB_NAME_=${EXP_NAME}/dms_r50_prune_e10_e_R34
 export PTH_NAME=epoch_10
 sh ./tools/slurm_train.sh eval test projects/configs/dms/ablation/super_size/dms_r50_prune_e10_e_R34.py work_dirs/${JOB_NAME_}
-sh ./tools/slurm_train.sh eval test projects/configs/dms/ablation/dms_r50_finetune.py work_dirs/${JOB_NAME_}_finetune
+sh ./tools/slurm_train.sh eval test projects/configs/dms/ablation/super_size/dms_r50_prune_e10_e_R34_finetune.py work_dirs/${JOB_NAME_}_finetune
 
 
 export JOB_NAME_=${EXP_NAME}/dms_r50_prune_e10_e_R101
 export PTH_NAME=epoch_10
 sh ./tools/slurm_train.sh eval test projects/configs/dms/ablation/super_size/dms_r50_prune_e10_e_R101.py work_dirs/${JOB_NAME_}
-sh ./tools/slurm_train.sh eval test projects/configs/dms/ablation/dms_r50_finetune.py work_dirs/${JOB_NAME_}_finetune
+sh ./tools/slurm_train.sh eval test projects/configs/dms/ablation/super_size/dms_r50_prune_e10_e_R101_finetune.py work_dirs/${JOB_NAME_}_finetune
 
 export JOB_NAME_=${EXP_NAME}/dms_r50_prune_e10_e_R152
 export PTH_NAME=epoch_10
 sh ./tools/slurm_train.sh eval test projects/configs/dms/ablation/super_size/dms_r50_prune_e10_e_R152.py work_dirs/${JOB_NAME_}
-sh ./tools/slurm_train.sh eval test projects/configs/dms/ablation/dms_r50_finetune.py work_dirs/${JOB_NAME_}_finetune
+sh ./tools/slurm_train.sh eval test projects/configs/dms/ablation/super_size/dms_r50_prune_e10_e_R152_finetune.py work_dirs/${JOB_NAME_}_finetune
