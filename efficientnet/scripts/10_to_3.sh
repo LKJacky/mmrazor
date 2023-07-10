52ce4989700ab6789b39e3a7072090491bb51cd6
Your Name <youremail@yourdomain.com>
2023-07-10 17:16:27 +0800

    update

--- efficientnet/scripts/10_to_3.sh
+++ efficientnet/scripts/10_to_3.sh
@@ -8,8 +8,8 @@ export GPUS_PER_NODE=4
 
 # total batch size=384*2=768=96*8
 
-sh timm_distributed_train.sh 8 timm_pruning.py ../data/imagenet_torch --model efficientnet_b3 -b 96 --sched step --epochs 30 \
+sh timm_distributed_train.sh 8 timm_pruning.py ../data/imagenet_torch --model efficientnet_b4 -b 96 --sched step --epochs 30 \
 --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf --opt-eps .001 -j 8 --warmup-lr 1e-6 --weight-decay 1e-5 \
---drop 0.3 --drop-path 0.2 --model-ema --model-ema-decay 0.9999 --aa rand-m9-mstd0.5 --remode pixel --reprob 0.2 --amp --lr .048 \
+--drop 0.4 --drop-path 0.2 --model-ema --model-ema-decay 0.9999 --aa rand-m9-mstd0.5 --remode pixel --reprob 0.2 --amp --lr .048 \
 --experiment 10_to_3 --pin-mem --resume output/train/10_to_3/last.pth.tar  --input-size 3 224 224 \
---target 0.348 --mutator_lr  0.0048 
+--target 0.223 --mutator_lr  0.0048
