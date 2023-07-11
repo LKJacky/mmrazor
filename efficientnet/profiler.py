e54f03ce1948a57805d471914c35b0814ddee8bf
Your Name <you@example.com>
2023-07-11 10:36:14 +0800

    update profiler

--- efficientnet/profiler.py
+++ efficientnet/profiler.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
     parser.add_argument('--model', type=str, default='')
     parser.add_argument('--alg', type=str, default='')
     parser.add_argument('--sub_alg', type=str, default='')
+    parser.add_argument('--size', type=int, default=224)
     args = parser.parse_args()
 
     model_name = args.model
@@ -30,10 +31,17 @@ if __name__ == "__main__":
             model = load_algo(model, args.sub_alg)
 
     res = ptflops.get_model_complexity_info(
-        model, (3, 224, 224),
+        model, (3, args.size, args.size),
         print_per_layer_stat=False,
         custom_modules_hooks={})
 
-    model = EffDmsAlgorithm(model)
+    model = EffDmsAlgorithm(
+        model,
+        mutator_kwargs=dict(
+            dtp_mutator_cfg=dict(
+                parse_cfg=dict(
+                    demo_input=dict(
+                        input_shape=(1, 3, args.size, args.size), ), )), ),
+    )
     print(model.mutator.info())
     print(res)
