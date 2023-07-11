fd058df9ce917babb573c9c550a8bc8720768558
Your Name <you@example.com>
2023-07-11 10:00:47 +0800

    update slurm

--- efficientnet/timm_slurm_train.sh
+++ efficientnet/timm_slurm_train.sh
@@ -6,7 +6,7 @@ PARTITION=$1
 JOB_NAME=$2
 GPUS=${GPUS:-8}
 GPUS_PER_NODE=${GPUS_PER_NODE:-8}
-CPUS_PER_TASK=${CPUS_PER_TASK:-5}
+CPUS_PER_TASK=${CPUS_PER_TASK:-15}
 SRUN_ARGS=${SRUN_ARGS:-""}
 PY_ARGS=${@:3}
 
@@ -20,4 +20,4 @@ srun -p ${PARTITION} \
     --cpus-per-task=${CPUS_PER_TASK} \
     --kill-on-bad-exit=1 \
     ${SRUN_ARGS} \
-    python -u timm_train.py data/imagenet_torch $PY_ARGS
+    python -u $PY_ARGS
