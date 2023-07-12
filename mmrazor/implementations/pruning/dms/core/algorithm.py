4d6bb28ce57fc0ebeeb18ccf6b840f248b64739c
Your Name <youremail@yourdomain.com>
2023-07-12 16:59:59 +0800

    update frame of expand ops

--- mmrazor/implementations/pruning/dms/core/algorithm.py
+++ mmrazor/implementations/pruning/dms/core/algorithm.py
@@ -13,6 +13,8 @@ from mmrazor.registry import MODELS, TASK_UTILS
 from mmrazor.utils import RuntimeInfo
 from .mutator import DMSMutator
 from .scheduler import DMSScheduler
+import copy
+from mmrazor.models.utils.expandable_utils import expand_expandable_dynamic_model
 
 
 def convert_float_to_tenosr(res: dict, device):
@@ -33,38 +35,43 @@ def update_dict_reverse(config1: dict, config2: dict):
 
 
 class DmsAlgorithmMixin():
+    default_mutator_kwargs = dict(
+        prune_qkv=False,
+        prune_block=False,
+        dtp_mutator_cfg=dict(
+            type='DTPAMutator',
+            channel_unit_cfg=dict(
+                type='DTPTUnit', default_args=dict(extra_mapping={})),
+            parse_cfg=dict(
+                _scope_='mmrazor',
+                type='ChannelAnalyzer',
+                demo_input=dict(
+                    type='DefaultDemoInput',
+                    input_shape=(1, 3, 224, 224),
+                ),
+                tracer_type='FxTracer'),
+        ))
+    default_scheduler_kargs = dict(
+        flops_target=0.8,
+        decay_ratio=0.8,
+        refine_ratio=0.2,
+        flop_loss_weight=1000,
+        structure_log_interval=10,
+        by_epoch=False,
+        target_scheduler='cos',
+    )
+    expand_unit_config = dict(
+        type='ExpandableUnit', default_args=dict(extra_mapping={})),
 
-    def __init__(
-        self,
-        model: nn.Module,
-        mutator_kwargs=dict(
-            prune_qkv=False,
-            prune_block=False,
-            dtp_mutator_cfg=dict(
-                type='DTPAMutator',
-                channel_unit_cfg=dict(
-                    type='DTPTUnit', default_args=dict(extra_mapping={})),
-                parse_cfg=dict(
-                    _scope_='mmrazor',
-                    type='ChannelAnalyzer',
-                    demo_input=dict(
-                        type='DefaultDemoInput',
-                        input_shape=(1, 3, 224, 224),
-                    ),
-                    tracer_type='FxTracer'),
-            ),
-            extra_module_mapping={}),
-        scheduler_kargs=dict(
-            flops_target=0.8,
-            decay_ratio=0.8,
-            refine_ratio=0.2,
-            flop_loss_weight=1000,
-            structure_log_interval=10,
-            by_epoch=False,
-            target_scheduler='cos',
-        )
-    ) -> None:
-        import copy
+    def __init__(self,
+                 model: nn.Module,
+                 mutator_kwargs={},
+                 scheduler_kargs={}) -> None:
+        mutator_kwargs = update_dict_reverse(
+            copy.deepcopy(self.default_mutator_kwargs), mutator_kwargs)
+        scheduler_kargs = update_dict_reverse(
+            copy.deepcopy(self.default_scheduler_kargs), scheduler_kargs)
+
         origin_model = copy.deepcopy(model)
         self.architecture = model
         self.mutator: DMSMutator = DMSMutator(**mutator_kwargs)
@@ -85,6 +92,28 @@ class DmsAlgorithmMixin():
     def to_static_model(self):
         self.model = self.architecture
         return to_static_model(self)
+
+    @classmethod
+    def expand_model(cls, model: nn.Module, ratio: float):
+        mutator_kargs = copy.deepcopy(cls.default_mutator_kwargs)
+        mutator_kargs['dtp_mutator_cfg'][
+            'channel_unit_cfg'] = cls.expand_unit_config
+        mutator = DMSMutator(**mutator_kargs)
+        mutator.prepare_from_supernet(model)
+        structure = mutator.dtp_mutator.choice_template
+        for key, num in structure.items():
+            unit = mutator.dtp_mutator._name2unit[key]
+            unit.expand_to(int(num * ratio))
+        model = expand_expandable_dynamic_model(model, zero=False)
+        return model
+
+    @classmethod
+    def show_structure(cls, model: nn.Module):
+        model = copy.deepcopy(model)
+        mutator = DMSMutator(**cls.default_mutator_kwargs)
+        mutator.prepare_from_supernet(model)
+        mutator.init_quick_flop(model)
+        print(mutator.info())
 
 
 @MODELS.register_module()
