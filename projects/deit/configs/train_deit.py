9825bb0de7bcb00832ee44eaed69b07ea7e334f3
Your Name <youremail@yourdomain.com>
2023-07-13 10:55:44 +0800

    update config

--- projects/deit/configs/train_deit.py
+++ projects/deit/configs/train_deit.py
@@ -1,5 +1,5 @@
-_base_ = 'mmcls::deit/deit-tiny_pt-4xb256_in1k.py'
+_base_ = 'mmcls::deit/deit-base_pt-16xb64_in1k.py'
 
-train_dataloader = dict(batch_size=32)
-test_dataloader = dict(batch_size=32)
-val_dataloader = dict(batch_size=32)
+# train_dataloader = dict(batch_size=32)
+# test_dataloader = dict(batch_size=32)
+# val_dataloader = dict(batch_size=32)
